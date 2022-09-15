d4b921534ddbec4087fd44a8502d59f07e4d5ae3
AsherSeibel <asherisrad@gmail.com>
2022-09-15 09:16:05 -0700

    First commit

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    //varibles
+    private float speed = 5.0f;
+    public float horizontalInput;
+    public float forwardInput;
+    public ParticleSystem explosionParticle;
+    public GameObject projectilePrefab;
+    // Start is called before the first fram update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per fram
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+            // Shoot Football
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+        // move the player forward
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+        // moves player forward
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        // moves player sideways
+        transform.Translate(Vector3.right * Time.deltaTime * speed * horizontalInput);
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Lava"))
+        {
+            explosionParticle.Play();
+        }
+    }
+}
---
+++ Assets/Scripts/SpawnEnemy.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnEnemy : MonoBehaviour
+{
+    public GameObject enemyPrefab;
+    private float spawnRange = 12;
+    private float startDelay = 3;
+    private float spawnInterval = 3f;
+    private float spawnRangeX = 12;
+    private float spawnRangeZ = 20;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        InvokeRepeating("SpawnEnemys", startDelay, spawnInterval);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+    void SpawnEnemys()
+    {
+
+       
+        Vector3 spawnRange = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnRangeZ);
+        Instantiate(enemyPrefab, spawnRange, enemyPrefab.transform.rotation);
+    }
+}
+
